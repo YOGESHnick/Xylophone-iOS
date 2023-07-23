bbae29f504cc6bc92e0b3b710a33f255ca2dd6cc
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-07-23 19:47:15 +0530

    added sounds to all buttons

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,13 +1,38 @@
+//import UIKit
+//
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+//
+//
+//    @IBAction func keyPressed(_ sender: UIButton) {
+//        print("pressed")
+//    }
+//
+//}
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
 
-    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
 
+    }
 }
-
