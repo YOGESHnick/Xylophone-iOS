28ec0b3f26d78a52784c415951d67d6fa58ffa11
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-07-23 20:48:59 +0530

    **Final Version **

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -26,11 +26,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+//        print(sender.currentTitle) //title of the button that was pressed
+        playSound(soundName: sender.currentTitle!) // '!' is included because currentTitle of button does'nt have to be string always
+        //Reduces the sender's (the button that got pressed) opacity to half.
+          sender.alpha = 0.5
+
+          //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
 
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(soundName:String ) {//soundName is the parameter,String is the datatype of the parameter required
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
 
